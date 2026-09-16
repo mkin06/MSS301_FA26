749f979dc98fc632dbebe627d3583d34fd5f30a5
Lê Ngọc Minh Kiên <lengocminhkien06@gmail.com>
2026-09-16 21:10:57 +0700

    feat(service): add createProduct and getAllProducts in ProductService

---
+++ Slot 3/ShoppingOnline/src/main/java/com/fpt/shoppingonline/service/ProductService.java
@@ -0,0 +1,43 @@
+package com.fpt.shoppingonline.service;
+
+import com.fpt.shoppingonline.dto.ProductRequest;
+import com.fpt.shoppingonline.dto.ProductResponse;
+import com.fpt.shoppingonline.model.Product;
+import com.fpt.shoppingonline.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductResponse createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .name(productRequest.name())
+                .description(productRequest.description())
+                .price(productRequest.price())
+                .build();
+
+        productRepository.save(product);
+        log.info("Product {} is saved", product.getId());
+
+        return new ProductResponse(product.getId(), product.getName(), product.getDescription(), product.getPrice());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        return productRepository.findAll()
+                .stream()
+                .map(product -> new ProductResponse(
+                        product.getId(),
+                        product.getName(),
+                        product.getDescription(),
+                        product.getPrice()))
+                .toList();
+    }
+}
